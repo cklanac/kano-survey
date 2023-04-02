9e94eb715c6a3d96fa51882127a87544b8968c31
Chris Klanac <1008777+cklanac@users.noreply.github.com>
2023-04-02 14:56:49 +0000

    add 2 questions per feature

--- src/Question.js
+++ src/Question.js
@@ -1,25 +1,52 @@
 import React from 'react';
-import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, Box, Typography } from '@mui/material';
+import {
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  Box,
+  Typography,
+  Grid,
+} from '@mui/material';
 
 const Question = ({ question }) => {
-  const { id, question: text } = question;
+  const { id, feature } = question;
+
+  const positive = "How would you feel if you had this feature?";
+  const negative = "How would you feel if you did not have this feature?";
 
   return (
     <Box mt={4}>
-      <FormControl component="fieldset">
-        <FormLabel component="legend">
-          <Typography variant="h6" gutterBottom>
-            {text}
-          </Typography>
-        </FormLabel>
-        <RadioGroup row name={`q${id}`}>
-          <FormControlLabel value="1" control={<Radio />} label="I like it" />
-          <FormControlLabel value="2" control={<Radio />} label="I expect it" />
-          <FormControlLabel value="3" control={<Radio />} label="I am neutral" />
-          <FormControlLabel value="4" control={<Radio />} label="I can tolerate it" />
-          <FormControlLabel value="5" control={<Radio />} label="I dislike it" />
-        </RadioGroup>
-      </FormControl>
+      <Typography variant="h6" gutterBottom>
+        {feature}
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <FormControl component="fieldset">
+            <FormLabel component="legend">{positive}</FormLabel>
+            <RadioGroup row name={`q${id}-positive`}>
+              <FormControlLabel value="1" control={<Radio />} label="I like it" />
+              <FormControlLabel value="2" control={<Radio />} label="I expect it" />
+              <FormControlLabel value="3" control={<Radio />} label="I am neutral" />
+              <FormControlLabel value="4" control={<Radio />} label="I can tolerate it" />
+              <FormControlLabel value="5" control={<Radio />} label="I dislike it" />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <FormControl component="fieldset">
+            <FormLabel component="legend">{negative}</FormLabel>
+            <RadioGroup row name={`q${id}-negative`}>
+              <FormControlLabel value="1" control={<Radio />} label="I like it" />
+              <FormControlLabel value="2" control={<Radio />} label="I expect it" />
+              <FormControlLabel value="3" control={<Radio />} label="I am neutral" />
+              <FormControlLabel value="4" control={<Radio />} label="I can tolerate it" />
+              <FormControlLabel value="5" control={<Radio />} label="I dislike it" />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
